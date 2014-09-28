f7fc1e0be342750c394529c632fe624f6d5c47a0
Christophe CHAUVET <christophe.chauvet@gmail.com>
2014-09-28 16:20:59 +0200

    [FIX] Fix Flake8 errors

--- product_brands.py
+++ product_brands.py
@@ -1,45 +1,48 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    product_brands module for OpenERP, Associate product with compatible brands
-#    Copyright (C) 2014 ozytwyst Julien Thomazeau <ozydev@julienthomazeau.fr>
+#   product_brands module for OpenERP, Associate product with compatible brands
+#   Copyright (C) 2014 ozytwyst Julien Thomazeau <ozydev@julienthomazeau.fr>
 #
-#    This file is a part of product_brands
+#   This file is a part of product_brands
 #
-#    product_brands is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#   product_brands is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 #
-#    product_brands is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#   product_brands is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 from openerp.osv import orm
 from openerp.osv import fields
 
+
 class product_brands(orm.Model):
-	_name = 'product.brands'
-	_columns = {
-		'name': fields.char('Brand Name'),
-		'description': fields.text('Description', translate=True),
-		'logo': fields.binary('Logo File')
-	}
+    _name = 'product.brands'
+    _columns = {
+        'name': fields.char('Brand Name'),
+        'description': fields.text('Description', translate=True),
+        'logo': fields.binary('Logo File')
+    }
+
 
 class product_template(orm.Model):
-	_inherit = 'product.template'
-	_columns = {
-		'brand_ids': fields.many2many(
-			'product.brands',
-			'product_brands_rel',
-			'template_id',
-			'brand_id',
-			'Brands')
-	}
-	
+    _inherit = 'product.template'
+    _columns = {
+        'brand_ids': fields.many2many(
+            'product.brands',
+            'product_brands_rel',
+            'template_id',
+            'brand_id',
+            'Brands')
+    }
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
